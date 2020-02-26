fe383f01b1b22935db0307bbeb8e34a0978522b0
Michal T <michaltarski@gmail.com>
2020-02-26 19:52:31 +0100

    [26.02.2020] Added three test files from untitled folder to check GIT commit from shell

---
+++ fizzbuzz.py
@@ -0,0 +1,20 @@
+import pytest
+print("Fizzbuzz game!")
+
+def fizzbuzz(a,b):
+    scope= list(range(a, b))
+    for i in scope:
+        if i % 3 == 0 and i % 5 == 0:
+            print(str(i) + " can be divided simultaneously per 3 and 5!")
+            print("FIZZBUZZ!")
+        elif i%3==0:
+            print(str(i) + " can be divided per 3!")
+            print("FIZZ!")
+        elif i%5==0:
+            print(str(i) + " can be divided per 5!")
+            print("BUZZ!")
+        else:
+            print("Child yells " + str(i) + "!")
+
+fizzbuzz(1,40)
+
