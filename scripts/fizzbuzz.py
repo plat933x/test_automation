df8d0092b68d466dcbf798b59af6633f423dacc4
MT <michaltarski@gmail.com>
2025-03-21 17:28:59 +0100

    project structure reorganized

---
+++ scripts/fizzbuzz.py
@@ -0,0 +1,21 @@
+print("Fizzbuzz game")
+print()
+# pare probnych linijek aby zrobic commit prosto z PYCHARM IDE
+
+def fizzbuzz(a,b):
+    scope= list(range(a, b))
+    for i in scope:
+        if i % 3 == 0 and i % 5 == 0:
+            print(str(i) + " can be divided simultaneously per 3 and 5!")
+            print("FIZZBUZZ!")
+        elif i%3==0:
+            print(str(i) + " can be divided per 3!")
+            print("FIZZ!")
+        elif i%5==0:
+            print(str(i) + " can be divided per 5!")
+            print("BUZZ!")
+        else:
+            print("Child yells " + str(i) + "!")
+
+fizzbuzz(5,25)
+
