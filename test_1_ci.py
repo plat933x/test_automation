44090f1dea91aa7709b3cf02681c0a3e8bf79dfb
MT <michaltarski@gmail.com>
2025-03-21 10:11:21 +0100

    tests renamed

---
+++ test_1_ci.py
@@ -0,0 +1,13 @@
+from counter import addder
+
+def test_addder_integer():
+    assert addder(5) == True
+
+def test_addder_string():
+    assert addder("koala") == False
+
+def test_addder_float():
+    assert addder(2.5) == False
+
+def test_addder_float():
+    assert addder(3.5) != True
