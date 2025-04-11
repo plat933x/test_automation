3b8822add75ae3dba6cac27ee07bef2b3c1d88e0
MT <michaltarski@gmail.com>
2025-04-11 21:06:50 +0200

    bhh

--- features/environment.py
+++ features/environment.py
@@ -5,20 +5,19 @@ from behave import fixture, use_fixture
 
 
 @fixture
-def browser(context):
+def selenium_browser_chrome(context):
     options = webdriver.ChromeOptions()
     options.add_argument("--headless")
-    options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
 
-    context.browser = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
-    context.browser.implicitly_wait(8)
+    service = ChromeService(executable_path=ChromeDriverManager().install())
+    context.browser = webdriver.Chrome(service=service, options=options)
     yield context.browser
     context.browser.quit()
 
-
 def before_all(context):
-    use_fixture(browser, context)
+    use_fixture(selenium_browser_chrome, context)
 
 def after_all(context):
     if hasattr(context, "browser"):
